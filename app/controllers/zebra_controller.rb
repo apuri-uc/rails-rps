10f4a824dfddb9c6f08a7548a7c4944d7a17b167
apuri-uc <apuri1@chicagobooth.edu>
2024-07-11 05:57:06 +0000

    updatie

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,48 @@
+class ZebraController < ApplicationController
+  def bird
+    render({ :template => "game_templates/homepage" })
+  end
+  
+  def giraffe
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+
+    render({ :template => "game_templates/play_rock" })
+  end
+
+  def elephant
+  @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+
+
+    render({ :template => "game_templates/play_paper" })
+  end 
+
+  def lion
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "scissors"
+      @outcome = "tied"
+    elsif @comp_move == "rock" 
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+
+    render({ :template => "game_templates/play_scissors" })
+  end
+end
